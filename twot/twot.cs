74362d7f66220d292ae250866dfacf4995542a04
Кирилл Мохирев <mokhirev.kirill2017@yandex.ru>
2022-12-05 17:50:55 +0500

    2 задание

--- twot/twot.cs
+++ twot/twot.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,30 @@ namespace twot
 {
     public partial class twot : Form
     {
+        public class ConnectDB
+        {
+
+            string sconn;
+
+            public string ReturnConn()
+            {
+                return sconn;
+            }
+            public ConnectDB(string a)
+            {
+                sconn = a;
+            }
+        }
+        ConnectDB connect = new ConnectDB("server=chuc.caseum.ru;port=33333;user=uchebka;database=uchebka;password=uchebka;");
+        MySqlConnection my;
         public twot()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
